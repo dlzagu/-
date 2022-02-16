6f594db8fad4be79ec26da99a9a2b8343134830d
dlzagu <eodnsdlekd@naver.com>
2022-02-16 12:27:04 +0900

    20220216:var,let,const

---
+++ jomad/app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "daeun";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello" + myName);
+
+myName = "kim"
+
+console.log("Your new name is " + myName);
